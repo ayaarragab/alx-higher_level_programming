ca1d2638de0a72be87380b8db2cbaf553003d32e
ayaarragab <ayarsk4343@gmail.com>
2023-08-11 09:34:42 +0300

    resolve

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
     import sys
+    if len(sys.argv) == 1:
+        print("0 arguments.")
     else:
         if len(sys.argv) == 2:
             print("1 argument:")
