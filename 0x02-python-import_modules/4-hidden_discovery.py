cf889abca23c7bf37303968cafcc5d324a2c38a9
ayaarragab <ayarsk4343@gmail.com>
2023-08-11 09:53:30 +0300

    pyc32

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,8 +2,7 @@
 if __name__ == '__main__':
     import hidden_4
     names = dir(hidden_4)
-    for arg in names:
-        for c in arg:
-            if c == '__':
-                continue
-        print('{}'.format(arg))
+    for i, arg in enumerate(names):
+        if arg[i] == '_':
+            continue
+        print(arg)
