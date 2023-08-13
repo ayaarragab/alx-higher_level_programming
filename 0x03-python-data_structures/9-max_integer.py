64f58eab82eabe95c1fb4b3ba154838025bdb5ce
ayaarragab <ayarsk4343@gmail.com>
2023-08-13 20:18:54 +0300

    d

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list == '':
+        return None
+    else:
+        max = my_list[0]
+        i = 0
+        while i < len(my_list) - 1:
+            if my_list[i + 1] > max:
+                max = my_list[i + 1]
+            i += 1
+        return max
