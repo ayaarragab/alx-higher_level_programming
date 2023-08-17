195023409f4e982966d4de8a161eccfd7e1dd4d7
ayaarragab <ayarsk4343@gmail.com>
2023-08-17 16:45:36 +0300

    to 10-best-score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    maxi = ''
+    maxi = 0
     for key, val in a_dictionary.items():
         maxi = key
         break
