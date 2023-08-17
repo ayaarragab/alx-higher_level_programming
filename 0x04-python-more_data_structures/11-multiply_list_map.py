804a858bbdd2232b877fcffbf87c80148673616c
ayaarragab <ayarsk4343@gmail.com>
2023-08-17 16:58:40 +0300

    map

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def mul_2(num):
     return num * 2
+
+
 def multiply_list_map(my_list=[], number=0):
-    new_list = list()
     new_list = map(mul_2, my_list)
-    return new_list
+    return list(new_list)
