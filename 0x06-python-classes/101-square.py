1a8d21b2da402fc70ab84249722105ba7f50b5d7
ayaarragab <ayarsk4343@gmail.com>
2023-08-30 20:59:03 +0300

    101^8 edit number of new lines

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -63,7 +63,7 @@ class Square:
         spacePatter = ' ' * self.position[0]
         totalStr = ''
         if self.size == 0:
-            return ''
+            print()
         else:
             if self.position[1] > 0:
                 for newline in range(self.position[1]):
