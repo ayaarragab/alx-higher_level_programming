82de0f6197117c7ad2660326a3baa70127388d49
ayaarragab <ayarsk4343@gmail.com>
2023-08-30 18:13:27 +0300

    2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""This module defines square class with size attribute
+with validation
+"""
+
+
+class Square:
+    """This is a square class with size private attribute with validation"""
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self._Square__size
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int):
+            if value >= 0:
+                self._Square__size = value
+            else:
+                raise ValueError('size must be >= 0')
+        else:
+            raise TypeError('size must be an integer')
+        
