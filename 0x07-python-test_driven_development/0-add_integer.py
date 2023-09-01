f44e7138b82bb1b25bf21f6673684383b0a286c1
ayaarragab <ayarsk4343@gmail.com>
2023-09-02 00:01:40 +0300

    First task

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+add_integer
+This module consists of an add function
+Functions: add(a, b): returns sum of two numbers
+"""
+def add_integer(a, b=98):
+    """ :param a: first arg :param b: second arg
+    :return: a + b
+    """
+    if not type(a) in (int, float):
+        raise TypeError('a must be an integer')
+    if not type(b) in (int, float):
+        raise TypeError('b must be an integer')
+    a = int(a)
+    b = int(b)
+    return a + b
