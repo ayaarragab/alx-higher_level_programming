dd34cc6cbd1b25f12adc653bebda8219fa57e7d2
ayaarragab <ayarsk4343@gmail.com>
2023-09-02 17:18:46 +0300

    fifth task

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''
+this module contains a function that prints a 
+text with 2 new lines
+after each of these characters: ., ? and :
+'''
+def text_indentation(text):
+    """
+    text_indentation(text): for each (., :, ?) => 2 newlines
+    """
+    newText = ""
+    in_whitespace = False #tracker for whitespace after the (., ?, :)
+    if not text or not isinstance(text, str):
+        raise TypeError('text must be a string')
+    for c in text:
+        if c == '.' or c == '?' or c == ':':
+            newText += "\n\n"
+            in_whitespace = True
+        elif c == ' ':
+            if not in_whitespace:
+                newText += c
+            in_whitespace = True
+        else:
+            in_whitespace = False
+            newText += c
+    print(newText)
