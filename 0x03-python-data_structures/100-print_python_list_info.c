311fe9497eab14d3c33f55291525292716aefbfd
ayaarragab <ayarsk4343@gmail.com>
2023-09-06 18:35:09 +0300

    0-cpython

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include </usr/include/python3.10/Python.h>
+#include </usr/include/python3.10/object.h>
+/**
+ * print_python_list_info - prints information about a python list
+ * @list: python object of class list
+ * Return: nothing
+*/
+void print_python_list_info(PyObject *list)
+{
+	PyListObject *l = (PyListObject *)list;
+	int i;
+
+	printf("[*] Size of the Python List = %ld\n\n", l->ob_base.ob_size);
+	printf("[*] Allocated = %ld\n\n", l->allocated);
+	for (i = 0; i < l->ob_base.ob_size; i++)
+		printf("Element %d: %s\n", i, l->ob_item[i]->ob_type->tp_name);
+}
