ffb6bb5672158840c2bed0e7d2eed09ef46e05db
ayaarragab <ayarsk4343@gmail.com>
2023-09-11 19:07:17 +0300

    1-my_list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This module includes a
+class MyList that inherits from list
+"""
+
+
+class MyList(list):
+    """
+    class MyList that inherits from list
+    """
+    def print_sorted(self):
+        """
+        print sorted list
+        """
+        self.sort()
+        print(self)
