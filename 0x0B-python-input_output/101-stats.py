8720312612debbfd3cfc9f62e5a6147e5fa74383
ayaarragab <ayarsk4343@gmail.com>
2023-09-13 22:11:53 +0300

    101

---
+++ 0x0B-python-input_output/101-stats.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""This module contains a script about
+log parsing"""
+
+
+import sys
+
+statusCodeDict = {'200': 0, '301': 0, '400': 0, '401': 0,
+                  '403': 0, '404': 0, '405': 0, '500': 0}
+totalSize = 0
+count = 0
+for line in sys.stdin:
+    if count == 9:
+        print(f"File size: {totalSize}")
+        for key in statusCodeDict.keys():
+            if statusCodeDict[key] == 0:
+                continue
+            print(f'{key}: {statusCodeDict[key]}')
+        count = 0
+        continue
+    lineContent = line.split()
+    statusCode = lineContent[-2]
+    fileSize = int(lineContent[-1])
+    totalSize += fileSize
+    if statusCode in statusCodeDict.keys():
+        statusCodeDict[statusCode] += 1
+    count += 1
