abf3f31872f325a0bea1bdd87483f67876f3ed9d
ayaarragab <ayarsk4343@gmail.com>
2023-09-13 17:46:31 +0300

    11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+This module contains class student
+"""
+
+
+class Student:
+    """
+    student class
+    """
+    def __init__(self, first_name, last_name, age):
+        """Initialization of the object
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        retrieves a dictionary
+        representation of a Student instance
+        """
+        myDic = dict()
+        allDic = self.__dict__
+        if attrs:
+            for item in attrs:
+                for key in list(allDic.keys()):
+                    if item is key:
+                        myDic[key] = allDic.get(key)
+                    else:
+                        continue
+            return (myDic)
+        elif attrs == []:
+            return myDic
+        return (allDic)
+
+    def reload_from_json(self, json):
+        self.first_name = json["first_name"]
+        self.last_name = json["last_name"]
+        self.age = json["age"]
+        return (self)
