dc14dd64ecf4e5adf8a9b5ffa03bd98ea2a6c41d
ayaarragab <ayarsk4343@gmail.com>
2023-09-13 22:37:09 +0300

    101

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -10,23 +10,24 @@ statusCodeDict = {'200': 0, '301': 0, '400': 0, '401': 0,
 totalSize = 0
 count = 0
 for line in sys.stdin:
-    lineContent = line.split()
-    statusCode = lineContent[-2]
-    fileSize = int(lineContent[-1])
-    totalSize += fileSize
-    if statusCode in statusCodeDict.keys():
-        statusCodeDict[statusCode] += 1
-    count += 1
+    if line:
+        lineContent = line.split()
+        statusCode = lineContent[-2]
+        fileSize = int(lineContent[-1])
+        totalSize += fileSize
+        if statusCode in statusCodeDict.keys():
+            statusCodeDict[statusCode] += 1
+        count += 1
 
-    if count == 10:
-        print(f"File size: {totalSize}")
-        for key in statusCodeDict.keys():
-            if statusCodeDict[key] == 0:
-                continue
-            print(f'{key}: {statusCodeDict[key]}')
-        count = 0
+        if count == 10:
+            print(f"File size: {totalSize}")
+            for key in statusCodeDict.keys():
+                if statusCodeDict[key] == 0:
+                    continue
+                print(f'{key}: {statusCodeDict[key]}')
+            count = 0
 
-if count > 0:
+if count > 0:  # This condition for if the input were less than 10
     print(f"File size: {totalSize}")
     for key in statusCodeDict.keys():
         if statusCodeDict[key] == 0:
