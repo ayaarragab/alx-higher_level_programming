d13e7e4790921d511a2e19f18af16b740865eb77
ayaarragab <ayarsk4343@gmail.com>
2023-09-13 17:03:02 +0300

    9

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+This module contains class student
+"""
+
+
+class student:
+    """
+    student class
+    """
+    def __init__(self, first_name, last_name, age):
+        """Initialization of the object
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        retrieves a dictionary
+        representation of a Student instance
+        """
+        return self.__dict__
