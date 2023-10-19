e5c2865b069291b6adaaf17fe611ba3d6b0d2510
ayaarragab <ayarsk4343@gmail.com>
2023-10-19 23:34:31 +0300

    4. ID must be unique

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,6 +1,5 @@
 -- a script that creates the table unique_id on your MySQL server
 CREATE TABLE IF NOT EXISTS id_not_null(
-    id INT NOT NULl DEFAULT 1,
+    id INT NOT NULl DEFAULT 1 UNIQUE,
     name VARCHAR(256),
-    PRIMARY KEY (id)
 );
