085e8a30e45cea73e14f055eb29b1820efbdfa15
ayaarragab <ayarsk4343@gmail.com>
2023-10-19 22:57:42 +0300

    1. Root user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- a script that creates the MySQL server user user_0d_1
 CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL TO user_0d_1@localhost
+GRANT *.* TO user_0d_1@localhost
