5d2fe0e68f4cd420290a3a3c137c364a0f5e91dc
ayaarragab <ayarsk4343@gmail.com>
2023-10-20 00:44:49 +0300

    10. Genre ID by show

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,4 @@
 -- a script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
-SELECT title, genre_id
+SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows, tv_show_genres
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 ORDER BY title ASC, genre_id ASC;
