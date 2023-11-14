4d88e33f2050a6a9ac5dba6a33abc18485728d8c
ayaarragab <ayarsk4343@gmail.com>
2023-11-14 21:18:25 +0200

    4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+module.exports =
+  class Rectangle {
+    constructor (w, h) {
+      if (w > 0 && h > 0) {
+        this.width = w;
+        this.height = h;
+      }
+    }
+
+    print () {
+      let pattern = '';
+      for (let index = 0; index < this.width; index++) {
+        pattern += 'X';
+      }
+      for (let index = 0; index < this.height; index++) {
+        console.log(pattern);
+      }
+    }
+
+    rotate () {
+      let temp;
+      temp = this.height;
+      this.height = this.width;
+      this.width = temp;
+    }
+
+    double () {
+      this.width = this.width * 2;
+      this.height = this.height * 2;
+    }
+  };
