b09e1d80d7bb4161e6f1d11973f25634e172dda1
ayaarragab <ayarsk4343@gmail.com>
2023-11-14 21:09:27 +0200

    2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,10 +2,9 @@
 module.exports =
   class Rectangle {
     constructor (w, h) {
-      if (!w || !h || w <= 0 || h <= 0) {
-        return {};
+      if (w > 0 && h > 0) {
+        this.width = w;
+        this.height = h;
       }
-      this.width = w;
-      this.height = h;
     }
   };
