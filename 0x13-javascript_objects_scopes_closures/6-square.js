6c5fa2400e53518a91d4b156f36e2374b60f0e6a
ayaarragab <ayarsk4343@gmail.com>
2023-11-14 21:36:34 +0200

    6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+const Rectangle = require('./4-rectangle');
+
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+  charPrint(c) {
+    if (c) {
+      let pattern = '';
+      for (let index = 0; index < this.width; index++) {
+        pattern += c;
+      }
+      for (let index = 0; index < this.height; index++) {
+        console.log(pattern);
+      }
+    } else {
+        super.print();
+    }
+  }
+};
