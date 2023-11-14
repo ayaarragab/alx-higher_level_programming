7227ed44e8e09163857ffecae30c1e9c6f8ea2a9
ayaarragab <ayarsk4343@gmail.com>
2023-11-14 18:00:10 +0200

    9

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const args = process.argv;
+
+if (!args[3]) {
+  console.log(NaN);
+}else {
+  add(args[2], args[3]);
+}
+function add (a, b) {
+  console.log(parseInt(a) + parseInt(b));
+}
