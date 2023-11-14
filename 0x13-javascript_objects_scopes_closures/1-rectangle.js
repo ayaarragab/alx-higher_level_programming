23281c018237750dbbdcf6e07bf7253619cb899c
ayaarragab <ayarsk4343@gmail.com>
2023-11-14 20:55:27 +0200

    1

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-module.exports = class rectangle {
-  constructor (w, h) {
-    this.width = w;
-    this.height = h;
-  }
-};
+module.exports = class Rectangle {
+    constructor (w, h) {
+      this.width = w;
+      this.height = h;
+    }
+  };
