fbe8606669f32e1a95fda8fa3bce4112e7e55b8d
ayaarragab <ayarsk4343@gmail.com>
2023-11-14 21:53:29 +0200

    8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -2,7 +2,7 @@
 exports.esrever = function (list) {
   const arr = [];
   let j = 0;
-  for (let index = list.length; index >= 0; index--) {
+  for (let index = list.length - 1; index >= 0; index--) {
     arr[j] = list[index];
     j += 1;
   }
