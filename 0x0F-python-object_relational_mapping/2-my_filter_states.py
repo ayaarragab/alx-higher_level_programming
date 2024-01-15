db011eb1518c166a790c65e6bafd7a493ab78142
ayaarragab <ayarsk4343@gmail.com>
2024-01-15 19:22:50 +0200

    2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -11,17 +11,17 @@ if __name__ == '__main__':
                 password=sys.argv[2],
                 database=sys.argv[3],
                 host="localhost",
-                port=3306,
-                 charset="utf8")
+                port=3306)
     cursor = connection.cursor()
     string = """SELECT * FROM states WHERE states.name = '{}'
              ORDER BY states.id ASC"""
     formatted = string.format(sys.argv[4])
-    cursor.execute(formatted)
-    query_rows = cursor.fetchall()
-    if query_rows:
-        for row in query_rows:
-            if row:
-                print(row)
+    if sys.argv[4]:
+        cursor.execute(formatted)
+        query_rows = cursor.fetchall()
+        if query_rows:
+            for row in query_rows:
+                if row:
+                    print(row)
     cursor.close()
     connection.close()
