d39e11265bfd8be82ee6a7d3ae61aa15095c915a
ayaarragab <ayarsk4343@gmail.com>
2024-01-16 12:21:14 +0200

    ORM 2

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -11,12 +11,14 @@ from sys import argv
 
 from model_state import State
 
-engine_str = f'mysql://{argv[1]}:{argv[2]}' + \
-             f'mysql://{argv[1]}:{argv[2]}@' + \
-             f'localhost:3306/{argv[3]}'
-engine = create_engine(engine_str)
-Session = sessionmaker(bind=engine)  # Object that its role to make a session
-session = Session()
-for row in session.query(State).order_by(State.id).all():
-    print(f'{row.id}: {row.name}')
-session.close()
+if __name__ == '__main__':
+    engine_str = f'mysql://{argv[1]}:{argv[2]}' + \
+                 f'mysql://{argv[1]}:{argv[2]}@' + \
+                 f'localhost:3306/{argv[3]}'
+    engine = create_engine(engine_str)
+    Session = sessionmaker(bind=engine)
+    # Object that its role to make a session
+    session = Session()
+    for row in session.query(State).order_by(State.id).all():
+        print(f'{row.id}: {row.name}')
+    session.close()
