0ead1eff53518541084e21aa8d32de026442f791
ayaarragab <ayarsk4343@gmail.com>
2024-01-27 07:54:16 -0800

    1

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 # comment explains everything ISA
-curl -sI "$1" -o text
-content_length=$(grep -i '^Content-Length:' text | awk '{print $2}')
-echo $content_length
-
+curl -sI "$1" -o text && content_length=$(grep -i '^Content-Length:' text | awk '{print $2}') && echo $content_length
