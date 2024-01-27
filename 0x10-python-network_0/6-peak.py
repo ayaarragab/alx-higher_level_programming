7f5dae751f395d90422425bc95023daeab92d627
ayaarragab <ayarsk4343@gmail.com>
2024-01-27 09:48:57 -0800

    6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+find peak
+"""
+
+def find_peak(list_of_integers):
+    """
+    a function that finds
+    a peak in a list of unsorted integers.
+    """
+    if not list_of_integers:
+        return None
+
+    low, high = 0, len(list_of_integers) - 1
+
+    while low < high:
+        mid = (low + high) // 2
+
+        if list_of_integers[mid] < list_of_integers[mid + 1]:
+            low = mid + 1
+        else:
+            high = mid
+
+    return list_of_integers[low]
+
