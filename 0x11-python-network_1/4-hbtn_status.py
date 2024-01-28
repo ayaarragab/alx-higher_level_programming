b2794811d09104cc16d3966bdc5f39d118560ee0
ayaarragab <ayarsk4343@gmail.com>
2024-01-28 14:52:40 +0200

    4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""
+a Python script that fetches
+https://alx-intranet.hbtn.io/status
+"""
+
+if __name__ == '__main__':
+    import requests
+
+    resp = requests.get('https://alx-intranet.hbtn.io/status')
+    print('Body response:')
+    print(f'\t- type: {type(resp.text)}')
+    print(f'\t- content: {resp.text}')
