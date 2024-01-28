3735d12adc0cd2d239965168b9091c9c725efd21
ayaarragab <ayarsk4343@gmail.com>
2024-01-28 16:01:31 +0200

    8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+"""
+Write a Python script that takes in a
+letter and sends a POST request to
+http://0.0.0.0:5000/search_user with
+the letter as a parameter
+"""
+
+if __name__ == '__main__':
+    from sys import argv
+    import requests
+
+    if argv[1]:
+        params = {'q': argv[1]}
+    else:
+        params = {'q': ""}
+    resp = requests.post('http://0.0.0.0:5000/search_user', data=params)
+
+    try:
+        json_data = resp.json()
+        if json_data:
+            print(f'[{json_data["id"]}] {json_data["name"]}')
+        else:
+            print("No result")
+    except ValueError:
+        print('Not a valid JSON')
