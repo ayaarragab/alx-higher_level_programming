724682c5a91dd2f19b7cc1c1ac7d661ceb55c73a
ayaarragab <ayarsk4343@gmail.com>
2024-01-28 10:40:07 +0200

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,11 +5,12 @@ https://alx-intranet.hbtn.io/status
 """
 
 
-from urllib import request, parse
+if __name__ == '__main__':
+    from urllib import request
 
 
-with request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-    print('Body response:')
-    print(f"    - type: {type(response.read())}")
-    print(f"    - content: {response.read()}")
-    print("    - utf8 content: " + response.read().decode("UTF-8"))
+    with request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        print('Body response:')
+        print(f"\t- type: {type(response.read())}")
+        print(f"\t- content: {response.read()}")
+        print("\t- utf8 content: " + response.read().decode("utf-8"))
