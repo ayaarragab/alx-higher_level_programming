c8ade6331e9046f805828bb47a8479b865c50e71
ayaarragab <ayarsk4343@gmail.com>
2024-01-28 15:08:22 +0200

    6. POST an email #1

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""
+a Python script that takes in a URL,
+sends a request to the URL and displays
+the value of the variable
+X-Request-Id in the response header
+"""
+
+if __name__ == '__main__':
+    from sys import argv
+    import requests
+
+    params = {
+        'email': argv[2]
+    }
+    res = requests.post(argv[1], params=params)
+    print(res.text)
