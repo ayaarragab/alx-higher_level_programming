8e4b6142b3a93095f80e321203ce740fa8edcd1f
ayaarragab <ayarsk4343@gmail.com>
2024-02-20 07:10:24 +0200

    0

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// This is a simple example of reading a file using the fs module.
+const fs = require('node:fs');
+const args = process.argv;
+fs.readFile(args[1], 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+});
