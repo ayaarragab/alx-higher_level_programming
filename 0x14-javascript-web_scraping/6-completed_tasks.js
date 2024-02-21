1c53af2113db81c8d6f0146d64818bd24869ff0f
ayaarragab <ayarsk4343@gmail.com>
2024-02-21 14:38:00 +0200

    62

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -3,10 +3,15 @@
 
 request = require("request");
 args = process.argv.slice(2);
+empty = {};
+
 
 request(args[0], function(error, response, body){
     if (error) {
 	console.log(error);
     }
-    console.log(body);
-})
+    for (let i = 0; i < JSON.parse(body).length; i++) 
+        if (JSON.parse(body)[i].completed === true) {
+        empty[i] = JSON.parse(body)[i].userId;
+        }    
+});
