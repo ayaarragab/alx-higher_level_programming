76ec76baf46da49fa929d1182dc42cbaabcf5b97
ayaarragab <ayarsk4343@gmail.com>
2024-02-21 13:35:28 +0200

    12

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,11 +2,10 @@
 // This is a simple example of reading a file using the fs module.
 const fs = require('node:fs');
 const args = process.argv;
-fs.writeFile(args[2], 'utf8', (err, data) => {
+fs.writeFile(args[2], args[3], 'utf8', err => {
   if (err) {
     console.error(err);
     return;
   }
   console.log(data);
 });
-
