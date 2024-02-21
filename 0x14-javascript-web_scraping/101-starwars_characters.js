8dac1c858d39f9d3cf2f9238f45410c646cf9122
ayaarragab <ayarsk4343@gmail.com>
2024-02-21 15:08:33 +0200

    101

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+// Write a script that prints all characters of a Star Wars movie:
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/';
+filmId = process.argv.slice(2)[0];
+
+request(url, function (error, _, body) {
+    if (error) {
+        console.log(error);
+    }
+    const films = JSON.parse(body).results;
+    for (const film of films) {
+        if (film.episode_id === Number(filmId)) {
+        for (const character of film.characters) {
+            request(character, function (error, response, body) {
+            if (error) {
+                console.log(error);
+            }
+            console.log(JSON.parse(body).name);
+            });
+        }
+        }
+    }
+});
