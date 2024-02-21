8832c2cb270116914691f32bac71180729f2ab72
ayaarragab <ayarsk4343@gmial.com>
2024-02-21 04:33:51 -0800

    61

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,2 +1,12 @@
 #!/usr/bin/node
 // This script lists all tasks that are completed
+
+request = require("request");
+args = process.argv.slice(2);
+
+request(args[0], function(error, response, body){
+    if (error) {
+	console.log(error);
+    }
+    console.log(body);
+})
