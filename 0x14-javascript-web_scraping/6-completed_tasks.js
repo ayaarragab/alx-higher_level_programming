36430b7915a2bb368bc983f0f4ffc4a4de7c8b9d
ayaarragab <ayarsk4343@gmail.com>
2024-02-21 14:46:07 +0200

    67

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,23 +1,23 @@
 #!/usr/bin/node
 // This script lists all tasks that are completed
 
-const request = require("request");
+const request = require('request');
 const args = process.argv.slice(2);
-let empty = {};
+const empty = {};
 
-request(args[0], function(error, _, body){
-    if (error) {
-        console.log(error);
+request(args[0], function (error, _, body) {
+  if (error) {
+    console.log(error);
+  }
+  const tasks = JSON.parse(body);
+  for (let i = 0; i < tasks.length; i++) {
+    if (tasks[i].completed === true) {
+      if (empty[tasks[i].userId]) {
+        empty[tasks[i].userId]++;
+      } else {
+        empty[tasks[i].userId] = 1;
+      }
     }
-    const tasks = JSON.parse(body);
-    for (let i = 0; i < tasks.length; i++) {
-        if (tasks[i].completed === true) {
-            if (empty[tasks[i].userId]) {
-                empty[tasks[i].userId]++;
-            } else {
-                empty[tasks[i].userId] = 1;
-            }
-        }    
-    }
-    console.log(empty);
+  }
+  console.log(empty);
 });
